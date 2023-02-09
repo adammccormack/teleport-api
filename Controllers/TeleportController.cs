1b4e66dc79c28b8f3ffd0bd9a07c60d69c7b83e3
Adam <adam.j.mccor@gmail.com>
2023-02-09 22:18:19 +0000

    added table view and table

--- Controllers/TeleportController.cs
+++ Controllers/TeleportController.cs
@@ -48,7 +48,44 @@ namespace DemoLibrary2.Controllers
         {
             var selectedContinent = model.SelectedContinent;
 
-            return RedirectToAction("Index");
+            return RedirectToAction("Table");
         }
+
+        public async Task<IActionResult> Table()
+        {
+
+            var continentItems = await ContinentsProcessor.LoadContinents();
+            var continents = continentItems._links.continentitems;
+
+
+            //var africaUA = await UrbanAreasProcessor.LoadAfricaUrbanAreas();
+            //var asiaUA = await UrbanAreasProcessor.LoadAsiaUrbanAreas();
+            var northAmericaUA = await UrbanAreasProcessor.LoadNorthAmericaUrbanAreas();
+            var northAmericaCities = northAmericaUA._links.uaitems;
+            var northAmericaScore = await ScoresProcessor.LoadNorthAmericaScores();
+            {
+                TeleportViewModel mymodel = new TeleportViewModel();
+                mymodel.Continents = continents;
+                mymodel.NorthAmericaCities = northAmericaCities;
+                mymodel.NorthAmericaScore = northAmericaScore;
+
+                mymodel.ContinentsSelectList = new List<SelectListItem>();
+
+                foreach (var continent in continents)
+                {
+                    mymodel.ContinentsSelectList.Add(new SelectListItem { Text = continent.name });
+                }
+                return View(mymodel);
+            }
+        }
+
+        // Your application should provide a drop-down list of continents,
+        // and when one is selected, an ordered table of maximum 5 rows with
+        // the following structure should be displayed
+
+        // when one is selected, send a HTTP request to the controller
+        // grab urban areas for continent "mymodel.NorthAmericaCities"
+        // take them to a new view called Table
+
     }
 }
