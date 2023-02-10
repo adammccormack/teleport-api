a80f5ef4d570f798a7a53db97ca75dd60dbf5145
Adam <adam.j.mccor@gmail.com>
2023-02-10 00:12:55 +0000

    refactored controller actions so that continents is only needed in one and UA scores etc in the other

--- Controllers/TeleportController.cs
+++ Controllers/TeleportController.cs
@@ -16,22 +16,15 @@ namespace DemoLibrary2.Controllers
         {
             ApiHelper.InitializeClient();
 
+            //TODO: Refactor all of this
+
             // move this type of logic to a helper function in continents processor, do the same where ever items are there.
             var continentItems = await ContinentsProcessor.LoadContinents();
             var continents = continentItems._links.continentitems;
 
-
-            //var africaUA = await UrbanAreasProcessor.LoadAfricaUrbanAreas();
-            //var asiaUA = await UrbanAreasProcessor.LoadAsiaUrbanAreas();
-            var northAmericaUA = await UrbanAreasProcessor.LoadNorthAmericaUrbanAreas();
-            var northAmericaCities = northAmericaUA._links.uaitems;
-            var northAmericaScore = await ScoresProcessor.LoadNorthAmericaScores();
-
             {
                 TeleportViewModel mymodel = new TeleportViewModel();
                 mymodel.Continents = continents;
-                mymodel.NorthAmericaCities = northAmericaCities;
-                mymodel.NorthAmericaScore = northAmericaScore;
 
                 mymodel.ContinentsSelectList = new List<SelectListItem>();
 
@@ -54,9 +47,6 @@ namespace DemoLibrary2.Controllers
         public async Task<IActionResult> Table()
         {
 
-            var continentItems = await ContinentsProcessor.LoadContinents();
-            var continents = continentItems._links.continentitems;
-
             //var africaUA = await UrbanAreasProcessor.LoadAfricaUrbanAreas();
             //var asiaUA = await UrbanAreasProcessor.LoadAsiaUrbanAreas();
             var northAmericaUA = await UrbanAreasProcessor.LoadNorthAmericaUrbanAreas();
@@ -65,16 +55,11 @@ namespace DemoLibrary2.Controllers
 
             {
                 TeleportViewModel mymodel = new TeleportViewModel();
-                mymodel.Continents = continents;
+
                 mymodel.NorthAmericaCities = northAmericaCities;
                 mymodel.NorthAmericaScore = northAmericaScore;
 
-                mymodel.ContinentsSelectList = new List<SelectListItem>();
 
-                foreach (var continent in continents)
-                {
-                    mymodel.ContinentsSelectList.Add(new SelectListItem { Text = continent.name });
-                }
                 return View(mymodel);
             }
         }
@@ -87,5 +72,13 @@ namespace DemoLibrary2.Controllers
         // put them into one object and pass into the view.
         // If I can do this for all the continents, put each top 5 cities, score and summary into their own objects, then I can update
         // the table dynamically.
+
+
+        //https://developers.teleport.org/api/reference/#/
+        // with this page, once I click on a city from the list, can perhaps get the request URL https://api.teleport.org/api/cities/?search=albuquerque
+        // then set the api call to be dynamic based on the city you click on, thereby making one api function that searches dynamically.
+
+        // still should choose top 5 cities for each continent first. 
+
     }
 }
