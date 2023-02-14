2c6015f0ad250b745c7d6a1cc55616ceb23cf388
Adam <adam.j.mccor@gmail.com>
2023-02-14 15:35:38 +0000

    refactored UrbanAreasProcessor

---
+++ Processors/UrbanAreasProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using DemoLibrary;
+using System.Security.Policy;
+using DemoLibrary2.Models;
+using Microsoft.DotNet.MSIdentity.Shared;
+using Newtonsoft.Json;
+using static DemoLibrary2.Models.UrbanAreasModel;
+
+namespace DemoLibrary2
+{
+	public class UrbanAreasProcessor
+	{
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadUrbanAreas(string url)
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var scores = JsonConvert.DeserializeObject<UrbanAreasModel.UrbanAreas.Root>(jsonString);
+                    return scores;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadAfricaUrbanAreas()
+        {
+            string africaUrl = "https://api.teleport.org/api/continents/geonames:AF/urban_areas/";
+            return await LoadUrbanAreas(africaUrl);
+        }
+
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadAsiaUrbanAreas()
+        {
+            string asiaUrl = "https://api.teleport.org/api/continents/geonames:AS/urban_areas/";
+            return await LoadUrbanAreas(asiaUrl);
+        }
+
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadSouthAmericaUrbanAreas()
+        {
+            string southAamericaUrl = "https://api.teleport.org/api/continents/geonames:SA/urban_areas/";
+            return await LoadUrbanAreas(southAamericaUrl);
+        }
+
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadNorthAmericaUrbanAreas()
+        {
+            string northAamericaUrl = "https://api.teleport.org/api/continents/geonames:NA/urban_areas/";
+            return await LoadUrbanAreas(northAamericaUrl);
+        }
+
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadOceaniaUrbanAreas()
+        {
+            string oceaniaUrl = "https://api.teleport.org/api/continents/geonames:OC/urban_areas/";
+            return await LoadUrbanAreas(oceaniaUrl);
+        }
+
+        public static async Task<UrbanAreasModel.UrbanAreas.Root> LoadEuropeUrbanAreas()
+        {
+            string europeUrl = "https://api.teleport.org/api/continents/geonames:EU/urban_areas/";
+            return await LoadUrbanAreas(europeUrl);
+        }
+    }
+}
