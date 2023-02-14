e1597c51417630ce150f299413daa18349aa6eb7
Adam <adam.j.mccor@gmail.com>
2023-02-14 19:36:44 +0000

    wip, refactoring minor error warnings

--- Models/ScoresModel.cs
+++ Models/ScoresModel.cs
@@ -7,29 +7,29 @@ namespace DemoLibrary2.Models
         {
             public class Category
             {
-                public string color { get; set; }
-                public string name { get; set; }
+                public string? color { get; set; }
+                public string? name { get; set; }
                 public double score_out_of_10 { get; set; }
             }
 
             public class Cury
             {
-                public string href { get; set; }
-                public string name { get; set; }
+                public string? href { get; set; }
+                public string? name { get; set; }
                 public bool templated { get; set; }
             }
 
             public class Links
             {
-                public List<Cury> curies { get; set; }
-                public Self self { get; set; }
+                public List<Cury>? curies { get; set; }
+                public Self? self { get; set; }
             }
 
             public class Root
             {
-                public Links _links { get; set; }
-                public List<Category> categories { get; set; }
-                public string summary { get; set; }
+                public Links? _links { get; set; }
+                public List<Category>? categories { get; set; }
+                public string? summary { get; set; }
                 public double teleport_city_score { get; set; }
 
                 public static implicit operator Root(List<Root> v)
@@ -40,7 +40,7 @@ namespace DemoLibrary2.Models
 
             public class Self
             {
-                public string href { get; set; }
+                public string? href { get; set; }
             }
         }
     }
